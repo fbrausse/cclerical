cf4f3eab6deeef8ba777102b34518fabb874a4cd
Franz Brauße <brausse@informatik.uni-trier.de>
2017-12-21 16:44:12 +0100

    std: intro bounded(x,n); simplifies sqrt.t17

--- std-irram.cc
+++ std-irram.cc
@@ -5,5 +5,6 @@ namespace cclerical {
 
 iRRAM::REAL real(iRRAM::INTEGER n) { return n; }
 iRRAM::REAL abs(iRRAM::REAL x) { return iRRAM::abs(x); }
+bool bounded(iRRAM::REAL x, iRRAM::INTEGER n) { return (bool)bound(x, int(-n)); }
 
 }
